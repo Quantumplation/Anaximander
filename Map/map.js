ec5b535cf39c35905cad2e93f2b3e028683ee849
Rob Kellett <rob@aorist.co>
2013-11-29 14:20:07 -0500

    You can now select which tick to view on the map

--- Map/map.js
+++ Map/map.js
@@ -1,10 +1,34 @@
 var paper, offset;
 
+function getTickList() {
+    $.get("http://quantumplation.me:4000/Anaximander", function(data) {
+        var select = $("#tick-select");
+        var t = select.val();
+        select.empty();
+        
+        var ticks = data.split("\n");
+        for (var i in ticks) {
+            if (ticks[i])
+                select.append("<option value='" + ticks[i] + "'>" + ticks[i] + "</option>");
+        }
+        
+        if (t)
+            select.val(t);
+        else
+            select.children(":last").attr("selected", "selected");
+        
+        loadAndDraw();
+    });
+}
+
 function loadAndDraw() {
     // here be the dragons
+    var tick = $("#tick-select").val();
+    if (!tick) return;
+    
     offset = {"x": document.documentElement.clientWidth / 2, "y": document.documentElement.clientHeight / 2};
-    $("svg > g *").remove();
-    $.getJSON("http://quantumplation.me:4000/Anaximander", function(data) {
+    $("svg > g").empty();
+    $.getJSON("http://quantumplation.me:4000/Anaximander/" + tick, function(data) {
         for (var i in data.report.stars) {
             var star = data.report.stars[i];
             paper.circle(star.x * 100 + offset.x, star.y * 100 + offset.y, 3).attr({"stroke": "gray", "stroke-width": "1", "fill": star.v == "1" ? "white" : "black"});
@@ -23,6 +47,6 @@ $(function() {
     svg.style.width = "100vw";
     svg.style.height = "100vh";
     
-    loadAndDraw();
-    $("#reload").click(loadAndDraw);
+    getTickList();
+    $("#reload").click(getTickList);
 });
